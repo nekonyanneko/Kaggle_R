c5207a0b23fead82e87ed40a32655ca5f1bf626a
nekonyanneko <morita.d.shun@gmail.com>
2017-04-06 00:11:38 +0900

    色々と修正。

--- titanic/script.R
+++ titanic/script.R
@@ -23,82 +23,194 @@ test$Age   <- matrix(test$Age)   %>% impute(what = "mean")
 # This temporary is used for conversion for Hist Gram
 hist_tmp <- train
 
+# ->>>>>> Attention male/female
 hist_tmp$Sex[hist_tmp$Sex == "male"]   <- 0
 hist_tmp$Sex[hist_tmp$Sex == "female"] <- 1
 hist(as.integer(hist_tmp$Sex[hist_tmp$Survived == 0]), breaks=2,
-     xlab ="Sex", ylab = "Survive", main = "Sex" , col = "#ff00ff50", labels = T)
+     xlab ="Sex", ylab = "Survive", main = "Sex" , col = "#ff00ff50", labels = T, ylim = c(0,500))
 hist(as.integer(hist_tmp$Sex[hist_tmp$Survived == 1]), breaks=2,
      xlab ="Sex", ylab = "Survive", main = "Sex" , col = "#00000050", labels = T, add = T)
 # ->>>>>> Attention male/female
-
+# ->>>>>> Attention Master Miss Mr Mrs
 hist_tmp$Title <- gsub('(.*, )|(\\..*)', '', hist_tmp$Name)
 table(hist_tmp$Sex, hist_tmp$Title)
-# Capt Col Don  Dr Jonkheer Lady Major Master Miss Mlle Mme  Mr Mrs  Ms Rev Sir the Countess
+#    Capt Col Don  Dr Jonkheer Lady Major Master Miss Mlle Mme  Mr Mrs  Ms Rev Sir the Countess
 # 0    1   2   1   6        1    0     2     40    0    0   0 517   0   0   6   1            0
 # 1    0   0   0   1        0    1     0      0  182    2   1   0 125   1   0   0            1
-hist(as.integer(hist_tmp$Sex[hist_tmp$Survived == 0]), breaks=2,
-     xlab ="Sex", ylab = "Survive", main = "Sex" , col = "#ff00ff50", labels = T)
-hist(as.integer(hist_tmp$Sex[hist_tmp$Survived == 1]), breaks=2,
-     xlab ="Sex", ylab = "Survive", main = "Sex" , col = "#00000050", labels = T, add = T)
-
-
+hist_tmp$Title[hist_tmp$Title == "Master"] <- 1
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 0 & hist_tmp$Title == "1"]), breaks=2,
+     xlab ="Master", ylab = "Survive", main = "Master" , col = "#ff00ff50", labels = T, ylim = c(0,25))
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 1 & hist_tmp$Title == "1"]), breaks=2,
+     xlab ="Master", ylab = "Survive", main = "Master" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It isn't a good result
+hist_tmp$Title[hist_tmp$Title == "Miss"] <- 2
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 0 & hist_tmp$Title == "2"]), breaks=2,
+     xlab ="Miss", ylab = "Survive", main = "Miss" , col = "#ff00ff50", labels = T, ylim = c(0,150))
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 1 & hist_tmp$Title == "2"]), breaks=2,
+     xlab ="Miss", ylab = "Survive", main = "Miss" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It's a good result
+hist_tmp$Title[hist_tmp$Title == "Mr"] <- 3
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 0 & hist_tmp$Title == "3"]), breaks=2,
+     xlab ="Mr", ylab = "Survive", main = "Mr" , col = "#ff00ff50", labels = T, ylim = c(0,520))
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 1 & hist_tmp$Title == "3"]), breaks=2,
+     xlab ="Mr", ylab = "Survive", main = "Mr" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It's a very good result
+hist_tmp$Title[hist_tmp$Title == "Mrs"] <- 4
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 0 & hist_tmp$Title == "4"]), breaks=2,
+     xlab ="Mrs", ylab = "Survive", main = "Mrs" , col = "#ff00ff50", labels = T, ylim = c(0,130))
+hist(as.integer(hist_tmp$Title[hist_tmp$Survived == 1 & hist_tmp$Title == "4"]), breaks=2,
+     xlab ="Mrs", ylab = "Survive", main = "Mrs" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It's a very good result
+# ->>>>>> Attention Master Miss Mr Mrs
+# ->>>>>> Attention Emarked
+hist_tmp$Embarked[hist_tmp$Embarked == "C"] <- 1
+hist(as.integer(hist_tmp$Embarked[hist_tmp$Survived == 0 & hist_tmp$Embarked == "1"]), breaks=2,
+     xlab ="C", ylab = "Survive", main = "C" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Embarked[hist_tmp$Survived == 1 & hist_tmp$Embarked == "1"]), breaks=2,
+     xlab ="C", ylab = "Survive", main = "C" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It is not a very good result
+hist_tmp$Embarked[hist_tmp$Embarked == "S"] <- 2
+hist(as.integer(hist_tmp$Embarked[hist_tmp$Survived == 0 & hist_tmp$Embarked == "2"]), breaks=2,
+     xlab ="C", ylab = "Survive", main = "S" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Embarked[hist_tmp$Survived == 1 & hist_tmp$Embarked == "2"]), breaks=2,
+     xlab ="C", ylab = "Survive", main = "S" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It is a good result
+hist_tmp$Embarked[hist_tmp$Embarked == "Q"] <- 3
+hist(as.integer(hist_tmp$Embarked[hist_tmp$Survived == 0 & hist_tmp$Embarked == "3"]), breaks=2,
+     xlab ="Q", ylab = "Survive", main = "Q" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Embarked[hist_tmp$Survived == 1 & hist_tmp$Embarked == "3"]), breaks=2,
+     xlab ="Q", ylab = "Survive", main = "Q" , col = "#00000050", labels = T, add = T)
+# ->>>>>> It is not a very good result
+# ->>>>>> Attention Emarked
+# ->>>>>> Attention SibSp/Parch
+hist(as.integer(hist_tmp$SibSp[hist_tmp$Survived == 0]), breaks=seq(0,10,1),
+     xlab ="SibSp", ylab = "Survive", main = "SibSp" , col = "#ff00ff50", labels = T, ylim = c(0,550))
+hist(as.integer(hist_tmp$SibSp[hist_tmp$Survived == 1]), breaks=seq(0,10,1),
+     xlab ="SibSp", ylab = "Survive", main = "SibSp" , col = "#00000050", labels = T, add = T)
+table(hist_tmp$Survived, hist_tmp$SibSp)
+hist(as.integer(hist_tmp$Parch[hist_tmp$Survived == 0]), breaks=seq(0,10,1),
+     xlab ="Parch", ylab = "Survive", main = "Parch" , col = "#ff00ff50", labels = T, ylim = c(0,550))
+hist(as.integer(hist_tmp$Parch[hist_tmp$Survived == 1]), breaks=seq(0,10,1),
+     xlab ="Parch", ylab = "Survive", main = "Parch" , col = "#00000050", labels = T, add = T)
+table(hist_tmp$Survived, hist_tmp$Parch)
+hist_tmp$SibSpAndParch <- hist_tmp$SibSp + hist_tmp$Parch
+hist(as.integer(hist_tmp$SibSpAndParch[hist_tmp$Survived == 0]), breaks=seq(0,10,1),
+     xlab ="SibSpAndParch", ylab = "Survive", main = "SibSpAndParch" , col = "#ff00ff50", labels = T, ylim = c(0,550))
+hist(as.integer(hist_tmp$SibSpAndParch[hist_tmp$Survived == 1]), breaks=seq(0,10,1),
+     xlab ="SibSpAndParch", ylab = "Survive", main = "SibSpAndParch" , col = "#00000050", labels = T, add = T)
+table(hist_tmp$Survived, hist_tmp$SibSpAndParch)
+# ->>>>>> Attention SibSp/Parch
+# ->>>>>> Attention Cabin
+hist_tmp$CabinA <- gsub('(A.*)', '0', hist_tmp$Cabin)
+hist_tmp$CabinB <- gsub('(B.*)', '1', hist_tmp$CabinA)
+hist_tmp$CabinC <- gsub('(C.*)', '2', hist_tmp$CabinB)
+hist_tmp$CabinD <- gsub('(D.*)', '3', hist_tmp$CabinC)
+hist_tmp$CabinE <- gsub('(E.*)', '4', hist_tmp$CabinD)
+hist_tmp$CabinF <- gsub('(F.*)', '5', hist_tmp$CabinE)
+hist_tmp$Cabin2 <- gsub('([G-Z].*)', '6', hist_tmp$CabinF)
+hist(as.integer(hist_tmp$Cabin2[hist_tmp$Survived == 0]), breaks=6,
+     xlab ="Cabin2", ylab = "Survive", main = "Cabin2" , col = "#ff00ff50", labels = T, ylim = c(0,550))
+hist(as.integer(hist_tmp$Cabin2[hist_tmp$Survived == 1]), breaks=6,
+     xlab ="Cabin2", ylab = "Survive", main = "Cabin2" , col = "#00000050", labels = T, add = T)
+# ->>>>>> last tuning
+# ->>>>>> Attention Cabin
+# ->>>>>> Attention Ticket
+table_tmp <- as.data.frame(table(hist_tmp$Survived,hist_tmp$Ticket))
+# ->>>>>> Attention Ticket
+# ->>>>>> Attention Pclass
+hist(as.integer(hist_tmp$Pclass[hist_tmp$Survived == 0 & hist_tmp$Pclass == "1"]), breaks=2,
+     xlab ="Pclass", ylab = "Survive", main = "Pclass" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Pclass[hist_tmp$Survived == 1 & hist_tmp$Pclass == "1"]), breaks=2,
+     xlab ="Pclass", ylab = "Survive", main = "Pclass" , col = "#00000050", labels = T, add = T)
+hist(as.integer(hist_tmp$Pclass[hist_tmp$Survived == 0 & hist_tmp$Pclass == "2"]), breaks=2,
+     xlab ="Pclass", ylab = "Survive", main = "Pclass" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Pclass[hist_tmp$Survived == 1 & hist_tmp$Pclass == "2"]), breaks=2,
+     xlab ="Pclass", ylab = "Survive", main = "Pclass" , col = "#00000050", labels = T, add = T)
+hist(as.integer(hist_tmp$Pclass[hist_tmp$Survived == 0 & hist_tmp$Pclass == "3"]), breaks=2,
+     xlab ="Pclass", ylab = "Survive", main = "Pclass" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Pclass[hist_tmp$Survived == 1 & hist_tmp$Pclass == "3"]), breaks=2,
+     xlab ="Pclass", ylab = "Survive", main = "Pclass" , col = "#00000050", labels = T, add = T)
+# ->>>>>> uum
+# ->>>>>> Attention Pclass
+# ->>>>>> Attention Pclass/Age
+hist_tmp$Age <- as.integer(hist_tmp$Age)
+hist_tmp$Age[0 <= hist_tmp$Age & hist_tmp$Age <= 9] <- 10
+hist_tmp$Age[10 <= hist_tmp$Age & hist_tmp$Age <= 19] <- 20
+hist_tmp$Age[20 <= hist_tmp$Age & hist_tmp$Age <= 29] <- 30
+hist_tmp$Age[30 <= hist_tmp$Age & hist_tmp$Age <= 39] <- 40
+hist_tmp$Age[40 <= hist_tmp$Age & hist_tmp$Age <= 49] <- 50
+hist_tmp$Age[50 <= hist_tmp$Age & hist_tmp$Age <= 59] <- 60
+hist_tmp$Age[60 <= hist_tmp$Age & hist_tmp$Age <= 69] <- 70
+hist_tmp$Age[70 <= hist_tmp$Age & hist_tmp$Age <= 79] <- 80
+hist_tmp$Age[80 <= hist_tmp$Age & hist_tmp$Age <= 89] <- 90
+hist_tmp$Age[90 <= hist_tmp$Age & hist_tmp$Age <= 99] <- 100
+hist(as.integer(hist_tmp$Age[hist_tmp$Survived == 0]), breaks=seq(10,100,10),
+     xlab ="Age", ylab = "Survive", main = "Age" , col = "#ff00ff50", labels = T, ylim = c(0,500))
+hist(as.integer(hist_tmp$Age[hist_tmp$Survived == 1]), breaks=seq(10,100,10),
+     xlab ="Age", ylab = "Survive", main = "Age" , col = "#00000050", labels = T, add = T)
+# ->>>>>> Attention Pclass/Age
 
 # Data Processing (Add Title)
 train$Title <- gsub('(.*, )|(\\..*)', '', train$Name)
 test$Title  <- gsub('(.*, )|(\\..*)', '', test$Name)
+# Data Processing (Add Label)
+train$Miss[train$Title == "Miss"] <- 1; train$Miss[is.na(train$Miss)] <- 0
+train$Mr[train$Title == "Mr"]     <- 1; train$Mr[is.na(train$Mr)]     <- 0
+train$Mrs[train$Title == "Mrs"]   <- 1; train$Mrs[is.na(train$Mrs)]   <- 0
+test$Miss[test$Title == "Miss"] <- 1; test$Miss[is.na(test$Miss)] <- 0
+test$Mr[test$Title == "Mr"]     <- 1; test$Mr[is.na(test$Mr)]     <- 0
+test$Mrs[test$Title == "Mrs"]   <- 1; test$Mrs[is.na(test$Mrs)]   <- 0
+train$Embarked_S[train$Embarked == "S"] <- 1; train$Embarked_S[is.na(train$Embarked_S)] <- 0
+test$Embarked_S[test$Embarked == "S"] <- 1; test$Embarked_S[is.na(test$Embarked_S)] <- 0
+train$SibSpAndParch <- train$SibSp + train$Parch
+test$SibSpAndParch <- test$SibSp + test$Parch
+train$CabinA <- gsub('(A.*)', '0', train$Cabin)
+train$CabinB <- gsub('(B.*)', '1', train$CabinA)
+train$CabinC <- gsub('(C.*)', '2', train$CabinB)
+train$CabinD <- gsub('(D.*)', '3', train$CabinC)
+train$CabinE <- gsub('(E.*)', '4', train$CabinD)
+train$CabinF <- gsub('(F.*)', '5', train$CabinE)
+train$Cabin2 <- gsub('([G-Z].*)', '6', train$CabinF)
+train$Cabin2[train$Cabin2 == ""] <- 0
+train <- train[,-which (colnames(train) %in% 
+                          c("CabinA","CabinB","CabinC","CabinD","CabinE","CabinF","CabinG"))]
+test$CabinA <- gsub('(A.*)', '0', test$Cabin)
+test$CabinB <- gsub('(B.*)', '1', test$CabinA)
+test$CabinC <- gsub('(C.*)', '2', test$CabinB)
+test$CabinD <- gsub('(D.*)', '3', test$CabinC)
+test$CabinE <- gsub('(E.*)', '4', test$CabinD)
+test$CabinF <- gsub('(F.*)', '5', test$CabinE)
+test$Cabin2 <- gsub('([G-Z].*)', '6', test$CabinF)
+test$Cabin2[test$Cabin2 == ""] <- 0
+test <- test[,-which (colnames(test) %in% 
+                          c("CabinA","CabinB","CabinC","CabinD","CabinE","CabinF","CabinG"))]
 
-# Show title counts by sex
-print(table(train$Sex, train$Title))
-print(table(test$Sex, test$Title))
-
-# Titles with very low cell counts to be combined to "rare" level
-mis_title  <- c('Dr', 'Ms', 'Dona')
-male_title <- c('Capt', 'Col', 'Don', 'Jonkheer', 'Major', 'Master', 'Rev', 'Sir')
-
-###################
-# Mr.→未婚既婚関係なく男性
-# Mrs.→既婚女性
-# Ms.→未婚既婚関係なく女性
-# Miss→未婚女性
-###################
-# Data Processing (Tittle)
-train$Title[train$Title == 'Dr' & train$Sex == 'male']  <- 'Mr'
-train$Title[train$Title == 'Lady']  <- 'Mrs'
-train$Title[train$Title == 'Mlle']  <- 'Miss'
-train$Title[train$Title == 'Mme']   <- 'Mrs'
-train$Title[train$Title == 'the Countess']  <- 'Mrs'
-train$Title[train$Title %in% male_title] <- 'Mr'
-train$Title[train$Title %in% mis_title] <- 'Ms'
-print(table(train$Sex, train$Title))
-
-test$Title[test$Title == 'Dr' & test$Sex == 'male']  <- 'Mr'
-test$Title[test$Title == 'Lady']  <- 'Mrs'
-test$Title[test$Title == 'Mlle']  <- 'Miss'
-test$Title[test$Title == 'Mme']   <- 'Mrs'
-test$Title[test$Title == 'the Countess']  <- 'Mrs'
-test$Title[test$Title %in% male_title] <- 'Mr'
-test$Title[test$Title %in% mis_title] <- 'Ms'
-print(table(test$Sex, test$Title))
-
-test[is.na(test[,"Fare"]),"Fare"] <- max(test[test$Pclass == 3, "Fare"], na.rm = T)
-
-train$mixPS <- train$Parch+train$SibSp
-test$mixPS <- test$Parch+test$SibSp
+# Fare/Age log scale
+train$Age <- log(train$Age)
+test$Age  <- log(test$Age)
+train$Age <- as.integer(train$Age)
+test$Age  <- as.integer(test$Age)
+train$Fare <- as.integer(train$Fare)
+test$Fare  <- as.integer(test$Fare)
+train$Fare <- log(train$Fare+1) # +1 is not -Inf
+test$Fare  <- log(test$Fare+1)  # +1 is not -Inf
 
 # Delete Data (PassengerId,Name)
-train <- train[,c(2,3,10,13,14)]
-test  <- test[,c(2,3,10,13,14)]
-train[,1] <- as.factor(train[,1])
-test[,1]  <- as.factor(test[,1])
-train[,4] <- as.factor(train[,4])
-test[,4]  <- as.factor(test[,4])
+train <- train[,-which (colnames(train) %in% 
+                          c("Name","Title","Cabin","Ticket","Embarked","PassengerId","SibSp","Parch"))]
+test  <- test[,-which  (colnames(test)  %in% 
+                          c("Name","Title","Cabin","Ticket","Embarked","PassengerId","SibSp","Parch"))]
+train[,"Survived"] <- as.factor(train[,"Survived"])
+test[,"Survived"]  <- as.factor(test[,"Survived"])
+train[,"Sex"] <- as.factor(train[,"Sex"])
+test[,"Sex"]  <- as.factor(test[,"Sex"])
 
 num <- sample(nrow(train), nrow(train)*0.7, replace = F)
 train1 <- train[num,]
 train2 <- train[-num,]
 
 # randomforest tuneRF
-tune <- tuneRF(train1[,-1], train1[,1], doBest=T, ntreeTry = 500, stepFactor = T)
+tune <- tuneRF(train1[,-1], train1[,1], doBest=T, ntreeTry = 1000, stepFactor = T)
 varImpPlot(tune)
 print("[tune-train1] Confusion Matrix:")
 print(tune$confusion)
